460f47c0ef7545f383692224249b64984b7b153f
William Ullholm <90192382+WilUll@users.noreply.github.com>
2022-11-03 11:18:49 +0100

    Two new attacks and code cleanup

---
+++ Source/Examensarbete/BTTask_AttackPlayer.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_AttackPlayer.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UBTTask_AttackPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
